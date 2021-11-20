15ba672357005384b56f0730739d289cedaac9f9
Martin Barri <maeertin@gmail.com>
2021-11-20 18:08:52 +0100

    chore: complete exercise 5

--- packages/exercise-5/src/script.js
+++ packages/exercise-5/src/script.js
@@ -4,11 +4,38 @@ import './style.css'
 // Scene
 const scene = new THREE.Scene()
 
-// Cube
-const geo = new THREE.BoxGeometry(1, 1, 1)
-const mat = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geo, mat)
-scene.add(mesh)
+/**
+ * Objects
+ */
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+)
+cube2.position.x = 2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+)
+cube3.position.x = -2
+group.add(cube3)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 // Sizes
 const sizes = {
