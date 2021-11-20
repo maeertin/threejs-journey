f6cc26521e6f8cc0bc5e26e98dae857dee239a1e
Martin Barri <maeertin@gmail.com>
2021-11-20 18:20:37 +0100

    chore: start exercise 6

---
+++ packages/exercise-6/src/script.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three'
+import './style.css'
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geo = new THREE.BoxGeometry(1, 1, 1)
+const mat = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geo, mat)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.set(0, 0, 3)
+scene.add(camera)
+
+// Renderer
+const canvas = document.getElementById('canvas')
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
