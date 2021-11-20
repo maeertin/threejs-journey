f0106b79b57100b0c9e204278da6ddab6356adbd
Martin Barri <maeertin@gmail.com>
2021-11-20 22:20:17 +0100

    chore: complete exercise 10

--- packages/exercise-10/src/script.js
+++ packages/exercise-10/src/script.js
@@ -2,17 +2,29 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import gsap from 'gsap'
+import * as dat from 'dat.gui'
 
 // Base
 const canvas = document.getElementById('canvas')
-const sizes = { width: window.innerWidth, height: window.innerHeight }
 const clock = new THREE.Clock()
 const scene = new THREE.Scene()
+const gui = new dat.GUI({ closed: true, width: 400 })
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+const parameters = {
+  color: 0xff0000,
+  spin: () => {
+    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + 10 })
+  },
+}
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: parameters.color }),
+)
 scene.add(mesh)
 
 // Camera
@@ -23,6 +35,15 @@ scene.add(camera)
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+
+// Debug
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation')
+gui.add(mesh.material, 'wireframe')
+gui.add(mesh, 'visible')
+gui.addColor(parameters, 'color').onChange(() => {
+  mesh.material.color.set(parameters.color)
+})
+gui.add(parameters, 'spin')
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
