c5c0d4f8e87cbb2bfc93fd728a4d101481a4d031
Martin Barri <maeertin@gmail.com>
2021-11-20 20:51:30 +0100

    chore: complete exercise 8

--- packages/exercise-8/src/script.js
+++ packages/exercise-8/src/script.js
@@ -4,21 +4,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 // Base
 const canvas = document.getElementById('canvas')
-
-// Sizes
-const sizes = {
-  width: 800,
-  height: 600,
-}
-
-/**
- * Cursor
- */
+const sizes = { width: window.innerWidth, height: window.innerHeight }
 const cursor = { x: 0, y: 0 }
-window.addEventListener('mousemove', (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5
-  cursor.y = -(event.clientY / sizes.height - 0.5)
-})
 
 // Scene
 const scene = new THREE.Scene()
@@ -31,8 +18,6 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-// const aspectRatio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.set(0, 0, 3)
 scene.add(camera)
@@ -46,6 +31,46 @@ const renderer = new THREE.WebGLRenderer({
   canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Resize
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Mousemove
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5
+  cursor.y = -(event.clientY / sizes.height - 0.5)
+})
+
+// Double click
+window.addEventListener('dblclick', () => {
+  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen()
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen()
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen()
+    }
+  }
+})
 
 // Clock
 const clock = new THREE.Clock()
