e1e7942c3cd7bb27545825ca6aebc65f03a5b959
Martin Barri <maeertin@gmail.com>
2021-11-21 12:11:42 +0100

    chore: start exercise 12

---
+++ packages/exercise-12/src/script.js
@@ -0,0 +1,60 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// Base
+const canvas = document.getElementById('canvas')
+const clock = new THREE.Clock()
+const scene = new THREE.Scene()
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+// Object
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(1, 1, 1)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Resize
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Animations
+function tick() {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  requestAnimationFrame(tick)
+}
+
+tick()
