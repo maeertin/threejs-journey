ed6e865cc544421d32db94b7e67e0bd44345310d
Martin Barri <maeertin@gmail.com>
2021-11-26 23:10:10 +0100

    chore: complete exercise 18

--- packages/18-particles/src/script.js
+++ packages/18-particles/src/script.js
@@ -19,12 +19,48 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/13.png')
 
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial())
-scene.add(cube)
+// Geometry
+// const particlesGeometry = new THREE.SpehereGeometry()
+const particleCount = 20000
+const particlesGeometry = new THREE.BufferGeometry()
+const particleColors = new Float32Array(particleCount * 3)
+const particlePositions = new Float32Array(particleCount * 3)
+for (let i = 0; i < particleCount * 3; i++) {
+  particleColors[i] = Math.random()
+  particlePositions[i] = (Math.random() - 0.5) * 10
+}
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(particleColors, 3))
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3))
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+  // Different alpha solutions
+  alphaMap: particleTexture,
+  // alphaTest: 0.001,
+  // depthTest: false,
+  depthWrite: false,
+  blending: THREE.AdditiveBlending,
+  // other...
+  color: '#ff88cc',
+  map: particleTexture,
+  size: 0.1,
+  // sizeAttenuation: false, // Detauls to true
+  transparent: true,
+  vertexColors: true,
+})
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+// Cube
+// const cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial())
+// scene.add(cube)
 
 /**
  * Sizes
@@ -76,6 +112,15 @@ const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  // Update particles
+  // particles.rotation.y = elapsedTime * 0.2
+  for (let i = 0; i < particles.geometry.attributes.position.count; i++) {
+    const i3 = i * 3
+    const x = particles.geometry.attributes.position.array[i3]
+    particles.geometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+  }
+  particles.geometry.attributes.position.needsUpdate = true
 
   // Update controls
   controls.update()
